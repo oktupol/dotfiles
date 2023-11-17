49a0ad360f35d3c70d0dc92e9ae495f47e20b997
Sebastian Wieland <git-commits@nwie.land>
2023-11-17 17:15:53 +0100

    kitty configurations

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -16,26 +16,27 @@ require("lazy").setup({
 	"nvim-lualine/lualine.nvim",
 	"nvim-tree/nvim-web-devicons",
 	{ "f-person/auto-dark-mode.nvim",
-	opts = {
-		update_interval = 1000,
-		set_dark_mode = function()
-			if vim.g.neovide then
-				vim.api.nvim_set_option("background", "dark")
-				vim.cmd("colorscheme base16-monokai")
-			else
-				vim.api.nvim_set_option("background", "dark")
-			end
-		end,
-		set_light_mode = function()
-			if vim.g.neovide then
-				vim.api.nvim_set_option("background", "light")
-				vim.cmd("colorscheme base16-humanoid-light")
-			else
-				vim.api.nvim_set_option("background", "light")
-			end
-		end,
+		opts = {
+			update_interval = 1000,
+			set_dark_mode = function()
+				if vim.g.neovide then
+					vim.api.nvim_set_option("background", "dark")
+					vim.cmd("colorscheme base16-monokai")
+				else
+					vim.api.nvim_set_option("background", "dark")
+				end
+			end,
+			set_light_mode = function()
+				if vim.g.neovide then
+					vim.api.nvim_set_option("background", "light")
+					vim.cmd("colorscheme base16-humanoid-light")
+				else
+					vim.api.nvim_set_option("background", "light")
+				end
+			end,
+		},
 	},
-},
+	"fladson/vim-kitty",
 })
 
 require('common')
