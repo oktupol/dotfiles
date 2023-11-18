62e58b6bab1feda770396954433b7fb548ff87e3
Sebastian Wieland <git-commits@nwie.land>
2023-11-18 13:57:15 +0100

    Added script to easyly set up symlinks

---
+++ create_symlinks.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+from_paths=(
+	".config"
+	".config/systemd/user"
+)
+
+for path in "${from_paths[@]}"; do
+	for f in $(find $path -mindepth 1 -maxdepth 1); do
+		basename="$(basename $f)"
+		absolute_path="$(realpath $f)"
+		link_target="$HOME/$f"
+
+		if [[ -L "$link_target" ]]; then
+			rm $link_target
+		fi
+		if [[ ! -e "$link_target" ]]; then
+			ln -s $absolute_path $link_target
+		fi
+	done
+done
