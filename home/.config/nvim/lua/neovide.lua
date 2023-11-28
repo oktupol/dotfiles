ac6b71d9b6cf6766f459a6d62a85370f74209191
Sebastian Wieland <git-commits@nwie.land>
2023-11-28 14:54:42 +0100

    Using JetBrains Mono font

--- home/.config/nvim/lua/neovide.lua
+++ home/.config/nvim/lua/neovide.lua
@@ -1,5 +1,5 @@
 if vim.g.neovide then
-	vim.o.guifont = "MesloLGS NF:h10"
+	vim.o.guifont = "JetBrainsMono Nerd Font:h10"
 	vim.opt.cursorline = true
 	vim.wo.number = true
 end
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -14,6 +14,7 @@ dependencies=(
 	slurp
 	mako
 	polkit-gnome
+	ttf-jetbrains-mono-nerd
 )
 
 sudo pacman -S ${dependencies[@]}
